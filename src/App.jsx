b391ee131eda2333d9ec84a4f22c5024f25d0403
Favio Valero <favio102@protonmail.com>
2024-05-21 08:14:59 -0700

    navbar, home, footer layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
 
 function App() {
-  return <>App</>;
+  return (
+    <Box width="400px" sx={{ width: { xl: "1448px" } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/exercise/:id" element={<ExerciseDetail />}></Route>
+      </Routes>
+      <Footer />
+    </Box>
+  );
 }
 
 export default App;
