b2b7c1aa0b96e3719e525411207ba160ae760593
Favio Valero <favio102@protonmail.com>
2024-05-25 23:28:35 -0700

    bodyPart, horizontalScrollBar

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,17 +1,10 @@
-import {
-  Box,
-  Button,
-  Stack,
-  TextField,
-  Typography
-} from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { fetchData, exerciseOptions } from "../utils/fetchData";
 import HorizontalScrollbar from "./HorizontalScrollbar";
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
   const [search, setSearch] = useState("");
-  const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
@@ -86,7 +79,11 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{ position: "relative", width: "100%", p: "20px" }}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar
+          data={bodyParts}
+          bodyPart={bodyPart}
+          setBodyPart={setBodyPart}
+        />
       </Box>
     </Stack>
   );
