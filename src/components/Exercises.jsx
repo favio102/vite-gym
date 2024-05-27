78730913dc7f99cb1df5a96b8d12dc300bd3c87d
Favio Valero <favio102@protonmail.com>
2024-05-27 04:57:11 -0700

    execises, horizontal bar added

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Box, Stack, Typography } from "@mui/material";
+import Pagination from "@mui/material/Pagination";
+import React, { useEffect, useState } from "react";
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
-const Exercises = () => {
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  console.log(exercises);
   return (
-    <div>Exercises</div>
-  )
-}
+    <Box id="exercises" sx={{ mt: { lg: "110px", xs: "50px" } }} p="20px">
+      <Typography variant="h3" mb="46px">
+        Showing Results
+      </Typography>
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: "110px", xs: "50px" } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {exercises.map((exercise, index) => (
+          <Typography key={exercise.id} variant="body1">{exercise.name}</Typography>
+        ))}
+      </Stack>
+    </Box>
+  );
+};
 
-export default Exercises
+export default Exercises;
