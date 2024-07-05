5a201aa783c041f76646d467b185cf6a3844f268
Favio V. F. <favio102@protonmail.com>
2024-07-05 08:37:02 -0700

    exercices component fixed

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,6 +1,6 @@
 import { Box, Stack, Typography } from "@mui/material";
 import Pagination from "@mui/material/Pagination";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 import ExerciseCard from "./ExerciseCard";
 import Loader from "./Loader";
@@ -9,18 +9,6 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [error, setError] = useState(null);
   const exercisesPage = 8;
-
-  const indexOfLastExercise = currentPage * exercisesPage;
-  const indexOfFirstExercise = indexOfLastExercise - exercisesPage;
-  const currentExercises = exercises.slice(
-    indexOfFirstExercise,
-    indexOfLastExercise
-  );
-
-  const paginate = (e, value) => {
-    setCurrentPage(value);
-    window.scrollTo({ top: 1800, behavior: "smooth" });
-  };
 
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -42,20 +30,38 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
       if (exercisesData) {
         setExercises(exercisesData);
       } else {
-        setError('No results. Please try again later.');
+        setError("No results. Please try again later.");
       }
     };
 
     fetchExercisesData();
   }, [bodyPart, setExercises]);
 
+  const indexOfLastExercise = currentPage * exercisesPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "10px", xs: "50px" } }} p="20px">
-      <Typography variant="h3" mb="46px">
+      <Typography
+        variant="h3"
+        sx={{ fontSize: { log: "44px", xs: "30px" } }}
+        mb="46px"
+      >
         Showing Results
       </Typography>
       {error ? (
-        <Typography variant="h6" color="error">{error}</Typography>
+        <Typography variant="h6" color="error">
+          {error}
+        </Typography>
       ) : !exercises.length ? (
         <Loader />
       ) : (
@@ -70,7 +76,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
               <ExerciseCard key={index} exercise={exercise} />
             ))}
           </Stack>
-          <Stack mt="100px" alignItems="center">
+          <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
             {exercises.length > 8 && (
               <Pagination
                 color="standard"
