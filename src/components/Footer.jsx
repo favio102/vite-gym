fade39abaa092ac3ea219fa98be930d764f10c5b
Favio V. F. <favio102@protonmail.com>
2024-07-05 09:06:29 -0700

    navbar & footer & icon update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,20 @@
-import { Box, Stack, Typography } from '@mui/material'
-import React from 'react'
-import Logo from '../assets/images/Logo-1.png'
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/logo.png";
 
 const Footer = () => {
   return (
-    <Box mt='80px' bgcolor='#fff3f4'>
-      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
-        <img src={Logo} alt="logo" width='200px' height='40px' />
-        <Typography variant='h5' pb='40px' mt='20px'>
-          Powered by Ciruc
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="5px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="200px" />
+        <Typography variant="h5" pb="10px" mt="10px" fontWeight='bold'>
+          Combines strength of warriors and steel
+        </Typography>
+        <Typography variant="h7" pb="30px" mt="5px">
+          Powered by Steel Warriors GYM
         </Typography>
       </Stack>
     </Box>
-  
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { Stack } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/images/Logo.png";
+import Logo from "../assets/images/logo.png";
 
 const Navbar = () => {
   return (
@@ -17,7 +17,7 @@ const Navbar = () => {
       <Link to="/">
         <img
           src={Logo}
-          style={{ width: "48px", height: "48ox", margin: "0 20px" }}
+          style={{ width: "60px", height: "60px", margin: "0 20px" }}
         />
       </Link>
       <Stack direction="row" gap="40px" fontSize="24px" alignItems="flex-end">
