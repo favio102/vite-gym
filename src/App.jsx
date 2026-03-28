363beeef2757424bb07f1456f0305ed7fff97423
Favio V. F. <favio102@protonmail.com>
2026-03-28 23:05:52 +0900

    Refactor App component to use lazy loading for Home and ExerciseDetail pages with Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import ExerciseDetail from "./pages/ExerciseDetail";
 import Footer from "./components/Footer";
+import Loader from "./components/Loader";
+
+const Home = lazy(() => import("./pages/Home"));
+const ExerciseDetail = lazy(() => import("./pages/ExerciseDetail"));
 
 function App() {
   return (
     <Box width="400px" sx={{ width: { xl: "1448px" } }} m="auto">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/exercise/:id" element={<ExerciseDetail />}></Route>
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/exercise/:id" element={<ExerciseDetail />}></Route>
+        </Routes>
+      </Suspense>
       <Footer />
     </Box>
   );
